b040ace600388e7f1736f9be0b905a66bfcd4db0
김경도 <dmsdpwls@sptek.co.kr>
2026-04-06 14:46:16 +0900

    123

--- Dockerfile
+++ Dockerfile
@@ -7,8 +7,8 @@ RUN mvn clean package -DskipTests
 FROM eclipse-temurin:17-jre
 
 WORKDIR /app
-COPY --from=build /app/target/*.jar app.jar
+COPY --from=build /app/target/*.war app.war
 
 EXPOSE 8080
 
-CMD ["java", "-jar", "app.jar"]
+CMD ["java", "-jar", "app.war"]
--- src/main/java/com/notify/NotifyApplication.java
+++ src/main/java/com/notify/NotifyApplication.java
@@ -1,16 +1,19 @@
 package com.notify;
 
-import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
-@EnableScheduling
-@MapperScan(basePackages = "com.notify.mapper")
 @SpringBootApplication
-public class NotifyApplication {
+public class NotifyApplication extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(NotifyApplication.class, args);
+    }
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(NotifyApplication.class);
     }
 }
